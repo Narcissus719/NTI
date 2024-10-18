2f580a1cf08391af5b8ed0656cd26bea46feb581
Narcissus719 <ebrahimramadan13@gmail.com>
2024-10-17 21:05:00 -0700

    s1

---
+++ index.js
@@ -0,0 +1,55 @@
+// let myArr=["name1","name2"]
+// myArr.push("name 3")
+// myArr.unshift("name 0")
+
+// for(let i =0;i<myArr.length;i++)
+// {
+//     console.log(`${i} =>${myArr[i]}`)
+// }
+
+// let userName = "Ebrahim Ramadan sayed ahmed"
+// let result = userName.split(` `)
+// console.log(result)
+// if(result.length=1&&result.length>3){
+//     console.log("Invalid name")  
+// }
+// else{
+//     console.log("Valid name")
+// }
+// let u = "ebrahimramadan"
+// let r = u.split("")
+// console.log(r)
+
+// let data=[1,2,3,4]
+// data.forEach((el,i,a)=>{
+// console.log(el)
+// }
+// )
+// let a1 = ["a","b","c","d","e"]
+// let a2= a1.splice(1,3)
+// console.log ( "a2 is ",a2)
+// let a3 = a1.concat(a2)
+// console.log("a3 is ",a3)
+
+// let d = [{name:"a"},{name:"b"}]
+// d = JSON.stringify(d)
+// localStorage.setItem("data",d)
+ 
+// let result = localStorage.getItem("data")
+// result = JSON.parse(result)
+// console.log(result)
+// console.log(result[0])
+
+// let m = document.getElementById("myDiv")
+//  m.textContent = "haklhsfklashvsv"
+
+// console.log(m)
+// let m = document.querySelector(".hh")
+// console.log(m.innerText)
+
+let mybtn = document.querySelector(".mybtn")
+mybtn.addEventListener("click",function(){
+    mybtn.classList.add(".ss") })
+
+
+
